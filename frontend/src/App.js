4bf1ba3ab70b4af640565296da1a40217cc7beaf
Mann-tech13 <mannshah.ict19@gmail.com>
2022-11-12 14:22:16 +0530

    Structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Dashboard from "./components/Dashboard/dashboard"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Dashboard/>}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ frontend/src/components/Dashboard/dashboard.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import "./dashboard.css"
+
+function Dashboard() {
+    return (
+        <div>
+            {/* <div className="container">
+                <div className="news">
+                    NEWS
+                </div>
+                <div className="ENTERTAINMENT">
+                    ENTERTAINMENT
+                </div>
+                <div className="technology">
+                    TECHNOLOGY
+                </div>
+                <div className="travel">
+                    TRAVEL
+                </div>
+                <div className="food">
+                    FOOD
+                </div>
+                <div className="sports">
+                    SPORTS
+                </div>
+            </div> */}
+            Hiii
+        </div>
+    )
+}
+
+export default Dashboard
